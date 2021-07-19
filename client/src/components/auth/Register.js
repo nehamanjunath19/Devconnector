6e100fb374edd55804968a30eba0229451ccdb28
Neha Manjunath <neha.sunema@gmail.com>
2021-07-19 15:47:40 +0530

    Initial commit

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,11 @@
 import React, { Fragment, useState } from 'react';
+import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
+import {setAlert} from '../../actions/alert';
+import {register} from '../../actions/auth';
+import PropTypes from 'prop-types';
 
-const Register = () => {
+const Register = ({setAlert, register}) => {
    const [formData, setFormData] = useState({
        name: '',
        email: '',
@@ -16,9 +20,9 @@ const Register = () => {
    const onSubmit = async e => {
        e.preventDefault();
        if(password !== password2){
-           console.log('Passwords do not match');
+           setAlert('Passwords do not match', 'danger');
        } else {
-           console.log("SUCCESS");
+           register({name, email, password});
     }
 };
 
@@ -42,8 +46,8 @@ const Register = () => {
           type="password"
           placeholder="Password"
           name="password"
-          minLength="6"
-          value={password} onChange={e => onChange(e)} required 
+          
+          value={password} onChange={e => onChange(e)} 
         />
       </div>
       <div className="form-group">
@@ -51,8 +55,8 @@ const Register = () => {
           type="password"
           placeholder="Confirm Password"
           name="password2"
-          minLength="6"
-          value={password2} onChange={e => onChange(e)} required 
+          
+          value={password2} onChange={e => onChange(e)} 
         />
       </div>
       <input type="submit" className="btn btn-primary" value="Register" />
@@ -64,4 +68,9 @@ const Register = () => {
     )
 };
 
-export default Register;
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired
+};
+
+export default connect(null, {setAlert, register})( Register);
